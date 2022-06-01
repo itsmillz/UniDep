f5033dd6c374dbf07626b08c13d059bdc70768c6
itsmillz <jorge.millar2601@gmail.com>
2022-05-31 23:26:26 -0400

    Proyecto Diseño

---
+++ prueba.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniDep</title>
+    <link rel="stylesheet" href="Estilos/estilos.css">
+</head>
+<body>
+    
+<div class="main-container">
+        <header id="main-header">
+            <div class="img-container">
+                <img src="imagenes/UniDep.jpg" alt="">
+            </div>
+            <nav class="navegacion">
+                <ul class="ul">
+                    <!-- <li><a href=""> <button class="button">Inicio</button></a></li> -->
+                    <!-- <li><a href=""><button class="button">Registro</button></a></li> -->
+                    <img src="imagenes/perfil-del-usuario.png" alt="">
+                    <li><a href=""><button>Publicar propiedad</button></a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="segundo-container">
+            <h1>
+                <strong>¡Estudia cómodo y seguro!</strong>
+            </h1>
+            <h2 class="texto">Encuentra el alojamiento que necesitas en el lugar que necesitas</h2>
+
+            <div class="flexsearch">
+                <div class="flexsearch--wrapper">
+                    <form class="flexsearch--form" action="#" method="post">
+                        <div class="flexsearch--input-wrapper">
+                            <input class="flexsearch--input" type="search"
+                                placeholder="Universidad: (Por favor, ingrese su universidad)">
+                        </div>
+                        <!-- <input type="submit" value="Filtrar por:"/> -->
+                        <input class="flexsearch--submit" type="submit" value="&#10140;" />
+                        <input class="flexsearch--submit2" type="submit"  value="&#9779;" />
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="depto">
+            <div class="dep-1">
+                <img src="imagenes/alquiler.jpg" alt="">
+            </div>
+            <div class="dep-2">
+                <div class="dep-22">
+                    <p>Departamento sector Universidad del Bío-Bío</p>
+                    <h2>Terrazas Collao 456</h2>
+                    <p>1 y 2 dormitorios, en los mejores sectores de concepción.
+                        Invierte hoy en tu futuro.
+                        Los mejores valores en UF y rentabilidad para tu inversión inmobiliaria.
+                    </p>
+                </div>
+                <div class="dep-23">
+
+                    <p>$400.000 CLP</p>
+                    <hr style="color: rgb(127, 77, 16);">
+                    <div class="contenedor-rate">
+                        <div class="accesorios">
+                                <!-- Imagenes de caracts de depto/propiedad -->
+                            <figure>
+                                <img src="imagenes/dormitorio.png" alt="Trulli">
+                                <figcaption>2 dorm</figcaption>
+                            </figure>
+
+                            <figure>
+                                <img src="imagenes/banera.png" alt="Trulli">
+                                <figcaption>1 baño</figcaption>
+                            </figure>
+
+                            <figure>
+                                <img src="imagenes/wifi.png" alt="Trulli">
+                                <figcaption>Wifi</figcaption>
+                            </figure>
+                        </div>
+                        <div class="puntuacion">
+                            <img src="imagenes/estrella.png" alt="">
+                            <p>4.7</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+</body>
+</html>
