45c36ef963d3c09f0d93fd6ca03f365a7495ed4b
itsmillz <jorge.millar2601@gmail.com>
2022-06-15 23:04:00 -0400

    Mejora en el diseño del formulario, menos texto y más comprensible al usuario

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,166 +16,180 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="../Estilos/estilos.css">
         <link rel="stylesheet" href="form.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 </head>
 
 <body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-
-    <header id="main-header" style="margin-bottom: -95px;">
-        <div class="img-container">
-            <img src="../imagenes/UniDep.jpg" alt="">
-        </div>
-        <nav class="navegacion">
-            <ul class="ul">
-                <img src="../imagenes/perfil-del-usuario.png" alt="">
-                <li><a href="prueba.php"><button>Publicar propiedad</button></a></li>
-            </ul>
-        </nav>
-    </header>
+    <div class="main-container">
+        <header class="main-header" id="main-header" >
+            <div class="img-container">
+                <img src="../imagenes/UniDep.jpg" alt="">
+            </div>
+            <nav class="navegacion">
+                <ul class="ul">
+                    <img src="../imagenes/perfil-del-usuario.png" alt="">
+                    <li><a href="prueba.php"><button>Publicar propiedad</button></a></li>
+                </ul>
+            </nav>
+        </header>
     
-    <div style="margin-top: 89px;" class="upfrm">
-        <?php if(!empty($statusMsg)){ ?>
-            <p class="status-msg"><?php echo $statusMsg; ?></p>
-        <?php } ?>
-        <!-- <p>Registro de las características de su propiedad</p> -->
-        <h1 class="h1">Registro de las características de su propiedad</h1>
-        <form name="form-work" class="was-validated mt-3 w-50 m-auto " style="background-color: #cacaca" action="../crud_propiedad/insert_propiedad.php" method="post" enctype="multipart/form-data">
-            <!-- <section class="form-registro" style="margin-top: 176px"> -->
-                
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="sector">Sector propiedad: </label>
-                    <input class=" form-control" type="text" placeholder="Ingrese sector de su propiedad" name="sector"
-                        required>
-                    <div class="invalid-feedback">Por favor, ingrese sector de su propiedad.</div>
-                </div>
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="precio">Precio propiedad: </label>
-                    <input class="form-control" type="number" placeholder="Ingrese el precio de arriendo" name="precio"
-                        required>
-                    <div class="invalid-feedback">Ingrese precio.</div>
-                </div>
-
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="propiedad">Tipo propiedad: </label>
-                    <input class="form-control" type="text" placeholder="Indique si su propiedad es casa o departamento" name="tipo"
-                        name="tipo" required>
-                    <div class="invalid-feedback">Ingrese tipo de propiedad.</div>
-                </div>
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="baño">Baños de su propiedad: </label>
-                    <input class="form-control" type="number"
-                        placeholder=" Ingrese cantidad de baño/s que posee su propiedad" name="baño" required>
-                    <div class="invalid-feedback">Ingrese cantidad de baños.</div>
-                </div>
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="habitacion">Habitaciones de su propiedad: </label>
-                    <input class="form-control" type="number"
-                        placeholder=" Ingrese la cantidad de habitaciones que posee su propiedad" name="habitacion" required>
-                    <div class="invalid-feedback">Ingrese cantidad de habitaciones.</div>
-                </div>
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="amoblada">¿Propiedad amoblada?: </label>
-                    <input class="form-control" type="boolean" placeholder=" Indique si es amoblada o no" name="amoblada" required>
-                    <div class="invalid-feedback">Indique si su propiedad es amoblada o no</div>
-                </div>
-
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="superficie">Cantidad de superficie propiedad: </label>
-                    <input class="form-control" type="number" placeholder=" Indique metros cuadrados aproximados" name="superficie" required>
-                    <div class="invalid-feedback">Indicar superficie propiedad</div>
-                </div>
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="superficie">¿Tiene estacionamiento?: </label>
-                    <input class="form-control" type="boolean" placeholder=" ¿Tiene estacionamiento? Responda con Si/No" name="estacionamiento" required>
-                    <div class="invalid-feedback">Indique si tiene estacionamiento</div>
-                </div>
-
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="servicio_aseo">¿Tiene servicio de aseo?: </label>
-                    <input class="form-control" type="boolean" placeholder="Responda con Si/No" name="servicio_aseo" required>
-                    <div class="invalid-feedback">Indique si tiene servicio de aseo</div>
-                </div>
-
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="gastos_comunes">Gastos comunes: </label>
-                    <input class="form-control" type="text" placeholder="Ingrese precio de gastos comunes" name="gastos_comunes" required>
-                    <div class="invalid-feedback">Indicar los gastos comunes</div>
-                </div>
-
-
-                <div class="col-12 mb-2 mt-2">
-                    <label class="form-label" for="descripcion">Describa la propiedad: </label>
-                    <input class="form-control" type="text" placeholder=" Indique otras características si lo estima necesario" name="descripcion" required>
-                    <div class="invalid-feedback">Descripción breve de su propiedad</div>
-                </div>
-
-                <p>Seleccione universidad:</p>
-                <?php include("../conexion_bd/conexion.php");?>
-                <div class="col-12 mb-2 mt-2">
-
-                    <?php
+        <div class="contain">
+            <?php if(!empty($statusMsg)){ ?>
+                <p class="status-msg"><?php echo $statusMsg; ?></p>
+            <?php } ?>
+            <!-- <p>Registro de las características de su propiedad</p> -->
+            <h1 class="h1">Registro de las características de su propiedad</h1>
+            <form name="form-work" class="was-validated formulario " action="../crud_propiedad/insert_propiedad.php" method="post" enctype="multipart/form-data">
+                <!-- <section class="form-registro" style="margin-top: 176px"> -->
                     
-                    $sql = "select * from universidad order by nombre_universidad";
-                    $resultado = mysqli_query($conn, $sql);
-                    $filas = mysqli_num_rows($resultado);
-                    if($filas){
-                        while($universidad = $resultado->fetch_row()){
-                            ?>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="check"value="<?php $universidad[0];?>"
-                            id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                            <?php echo $universidad[1]?>
-                        </label>
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="sector">Sector propiedad: </label>
+                        <input class=" form-control" type="text" placeholder="Ingrese sector de su propiedad" name="sector"
+                            required>
+                        <!-- <div class="invalid-feedback">Por favor, ingrese sector de su propiedad.</div> -->
                     </div>
-                    <?php
-                        }
-                    }
-                ?>
-                
-                </div>
-                <!-- <div class="form-group"> -->
-                    <!-- <br>
-                    <label>Select Image Files to Upload:</label>
-                    <input type="file" name="image[]" multiple>
-                    <input type="submit" name="submit" value="UPLOAD"> -->
-                    <!-- <label>Imagen Uno</label> -->
-                    <!-- <input type="file" name="image[]" class="form-control" multiple/> -->
-                <!-- </div> -->
 
-                <!-- <div class="form-group">
-                    <label>Imagen Dos</label>
-                    <input type="file" name="image[]" class="form-control"/>
-                </div>
-                <div class="form-group">
-                    <label>Imagen Tres</label>
-                    <input type="file" name="image[]" class="form-control"/>
-                </div>
-                <div class="form-group">
-                    <label>Imagen Cuatro</label>
-                    <input type="file" name="image[]" class="form-control"/>
-                </div>
-                <div class="form-group">
-                    <label>Imagen Cinco</label>
-                    <input type="file" name="image[]" class="form-control"/>
-                </div> -->
-                <!-- <input type="submit" name="submit" value="submit" class="btn btn-primary"> -->
-                <p>Revise que todos los datos sean correctos</p>
-                <input class="boton" type="submit" name="submit" value="Siguiente"> 
-            <!-- </section> -->
-        </form>
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="precio">Precio propiedad: </label>
+                        <input class="form-control" type="number" placeholder="Ingrese el precio de arriendo" name="precio"
+                            required>
+                        <!-- <div class="invalid-feedback">Ingrese precio.</div> -->
+                    </div>
+
+
+                    <div class="col-12 mb-2 mt-2">
+                        
+                        <label class="form-label" for="propiedad">Tipo propiedad: </label>
+                        <!--
+                        <input class="form-control" type="text" placeholder="Indique si su propiedad es casa o departamento" name="tipo"
+                            name="tipo" required>
+                        -->
+                        <select class="form-control" required name="" id="">
+                                <option value="">Seleccione un tipo de propiedad</option>
+                                <option value="1">Habitación</option>
+                                <option value="2">Casa</option>
+                                <option value="3">Departamento</option>
+                                <option value="4">Pensión</option>
+                        </select>
+                        <!-- <div class="invalid-feedback">Ingrese tipo de propiedad.</div> -->
+                    </div>
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="baño">Baños de su propiedad: </label>
+                        <input class="form-control" type="number"
+                            placeholder=" Ingrese cantidad de baño/s que posee su propiedad" name="baño" required>
+                        <!-- <div class="invalid-feedback">Ingrese cantidad de baños.</div> -->
+                    </div>
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="habitacion">Habitaciones de su propiedad: </label>
+                        <input class="form-control" type="number"
+                            placeholder=" Ingrese la cantidad de habitaciones que posee su propiedad" name="habitacion" required>
+                        <!-- <div class="invalid-feedback">Ingrese cantidad de habitaciones.</div> -->
+                    </div>
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="amoblada">¿Propiedad amoblada?: </label>
+                        <!--
+                        <input class="form-control" type="boolean" placeholder=" Indique si es amoblada o no" name="amoblada" required>-->
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio1" required="required" name="optradio" value="option1" > Si
+                            <label class="form-check-label" for="radio1"></label>
+                            
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">No
+                            <label class="form-check-label" for="radio2"></label>
+                            <!-- <div class="invalid-feedback">Por favor marque una opción</div> -->
+                        </div>
+                    
+                    </div>
+
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="superficie">Cantidad de superficie propiedad: </label>
+                        <input class="form-control" type="number" placeholder=" Indique metros cuadrados aproximados" name="superficie" required>
+                        <!-- <div class="invalid-feedback">Indicar superficie propiedad</div> -->
+                    </div>
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="superficie">¿Tiene estacionamiento?: </label>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio1" required="required" name="optradio" value="option1" > Si
+                            <label class="form-check-label" for="radio1"></label>
+                            
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">No
+                            <label class="form-check-label" for="radio2"></label>
+                            <!-- <div style="left:0 ;" class="invalid-feedback">Por favor marque una opción</div> -->
+                        </div>
+                    </div>
+
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="servicio_aseo">¿Tiene servicio de aseo?: </label>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio1" required="required" name="optradio" value="option1" > Si
+                            <label class="form-check-label" for="radio1"></label>
+                            
+                        </div>
+                        <div class="form-check">
+                            <input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">No
+                            <label class="form-check-label" for="radio2"></label>
+                            <!-- <div class="invalid-feedback">Por favor marque una opción</div> -->
+                        </div>
+                    </div>
+
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="gastos_comunes">Gastos comunes: </label>
+                        <input class="form-control" type="text" placeholder="Ingrese precio de gastos comunes" name="gastos_comunes" required>
+                        <!-- <div class="invalid-feedback">Indicar los gastos comunes</div> -->
+                    </div>
+
+
+                    <div class="col-12 mb-2 mt-2">
+                        <label class="form-label" for="descripcion">Describa la propiedad: </label>
+                        <textarea name="descripcion" class="form-control"  placeholder=" Indique otras características si lo estima necesario" required ></textarea>
+                        <!-- <div class="invalid-feedback">Descripción breve de su propiedad</div> -->
+                    </div>
+
+                    <p>Seleccione universidad:</p>
+                    <?php include("../conexion_bd/conexion.php");?>
+                    <div class="col-12 mb-2 mt-2">
+
+                        <?php
+                        
+                        $sql = "select * from universidad order by nombre_universidad";
+                        $resultado = mysqli_query($conn, $sql);
+                        $filas = mysqli_num_rows($resultado);
+                        if($filas){
+                            while($universidad = $resultado->fetch_row()){
+                                ?>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="check"value="<?php $universidad[0];?>"
+                                id="flexCheckChecked">
+                            <label class="form-check-label" for="flexCheckChecked">
+                                <?php echo $universidad[1]?>
+                            </label>
+                        </div>
+                        <?php
+                            }
+                        }
+                    ?>
+                    
+                    </div>
+                    <p class="alert alert-warning text-center ">Revise que todos los datos sean correctos</p>
+                    <div class="w-100 row">
+                        <input class="btn btn-success w-75 m-auto" type="submit" name="submit" value="Siguiente"> 
+                    </div>
+            </form>
+        </div>
     </div>
 </body>
 </html>
