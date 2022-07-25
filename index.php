a453536d7ccebc9dfcb732e52c4945e1b777c961
Crisandru <crisandru1997@gmail.com>
2022-07-25 10:27:04 -0400

    Corrección de estilos en el index

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             <div class="contenedor-principal-buscador">
                 <div class="contenedor-secundario-buscador">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="rgb(102, 102, 102);" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z" fill="#626262"/></svg>
-                    <input onkeyup="buscar_ahora($('#buscar').val());" type="search" name="buscar" id="buscar" value="" placeholder="Ingrese su universidad para mostrar arriendos cercanos">
+                    <input onkeyup="buscar_ahora($('#buscar').val());" type="search" name="buscar" id="buscar" value="" placeholder="Ingrese su universidad para mostrar arriendos cercanos" pattern="[a-zA-Z-]{47}" maxlength="47" minlength="0">
                 </div>
             </div>
         </div>
@@ -54,23 +54,23 @@
                         <label for="precio" class="titulo-filtro">Precio</label>
                         <div class="filtro-precio-interior">
                             <div class="precio-desde">
-                                <input type="number" name="desde" value="<?php echo $_POST["desde"]; ?>" placeholder="Desde">
+                                <input type="number" name="desde" value="<?php echo $_POST["desde"]; ?>" placeholder="Desde" min="0" max="1500000" pattern="[0-9]{7}" title="Ingrese un precio valido.">
                             </div>
                             <div class="precio-hasta">
-                                <input type="number" name="hasta" value="<?php echo $_POST["hasta"]; ?>" placeholder="Hasta">
+                                <input type="number" name="hasta" value="<?php echo $_POST["hasta"]; ?>" placeholder="Hasta" min="0" max="1500000" pattern="[0-9]{7}" title="Ingrese un precio valido.">
                             </div>
                         </div>
                     </section>
                     <section class="filtro-general-input">
                         <label for="banos" class="titulo-filtro">Baños</label>
-                        <input type="number" name="banos" value="<?php echo $_POST["banos"]; ?>">
+                        <input type="number" name="banos" value="<?php echo $_POST["banos"]; ?>" placeholder="Cantidad de baños" min="0" max="8" pattern="[0-8]{1}">
                     </section>
                     <section class="filtro-general-input">
-                        <label for="habitaciones" class="titulo-filtro">Habitaciones</label>
-                        <input type="number" name="habitaciones" value="<?php echo $_POST["habitaciones"]; ?>">
+                        <label for="habitaciones" class="titulo-filtro">Dormitorios</label>
+                        <input type="number" name="habitaciones" value="<?php echo $_POST["habitaciones"]; ?>" placeholder="Cantidad de dormitorios" min="0" max="8" pattern="[0-8]{1}">
                     </section>
                     <section class="filtro-select">
-                        <label class="titulo-filtro">amoblada</label>
+                        <label class="titulo-filtro">Amoblada</label>
                         <select name="amoblada" id="amoblada">
                                 <option value="" selected>Seleccionar</option>
                                 <option value="1">Si</option>
@@ -89,10 +89,10 @@
                         <label for="gastos-comunes" class="titulo-filtro">Gastos Comunes</label>
                         <div class="filtro-precio-interior">
                             <div class="precio-desde">
-                                <input type="number" name="gastoscomunesdesde" value="<?php echo $_POST["gastoscomunesdesde"]; ?>" placeholder="Desde">
+                                <input type="number" name="gastoscomunesdesde" value="<?php echo $_POST["gastoscomunesdesde"]; ?>" placeholder="Desde" min="0" max="150000" pattern="[0-9]{6}">
                             </div>
                             <div class="precio-hasta">
-                                <input type="number" name="gastoscomuneshasta" value="<?php echo $_POST["gastoscomuneshasta"]; ?>" placeholder="Hasta">
+                                <input type="number" name="gastoscomuneshasta" value="<?php echo $_POST["gastoscomuneshasta"]; ?>" placeholder="Hasta" min="0" max="150000" pattern="[0-9]{6}">
                             </div>
                         </div>
                     </section>
