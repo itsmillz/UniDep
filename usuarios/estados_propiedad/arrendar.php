a100a06de78ca7e9056e5cd3673bbb3eed95f57a
Cristian Soto Mancilla <78458281+Crisandru1997@users.noreply.github.com>
2022-09-05 01:20:01 -0300

    Delete usuarios directory

--- usuarios/estados_propiedad/arrendar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../../db_connection/connection.php';
-    $id = $_GET['id'];
-    $sql = "UPDATE propiedad SET estado = '1' WHERE id_propiedad='$id'";
-    $conn->query($sql);
-    header("Location: ../mis_propiedades.php");
-?>
--- usuarios/estados_propiedad/eliminar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../../db_connection/connection.php';
-    $id = $_GET['id'];
-    $sql = "DELETE FROM propiedad WHERE id_propiedad='$id'";
-    $conn->query($sql);
-    header("Location: ../mis_propiedades.php");
-?>
