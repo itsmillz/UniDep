26f88fb68c8a82b28ae212ac8901f9c9eb6ca768
Cristian Soto Mancilla <78458281+Crisandru1997@users.noreply.github.com>
2022-09-06 00:21:21 -0300

    Update connection.php

--- www/db_connection/connection.php
+++ www/db_connection/connection.php
@@ -2,17 +2,17 @@
 
     // Conexión base de datos de la Universidad.
     // * Se requiere estar conectado al OpenVPN.
-    $conn=mysqli_connect('mysqltrans.face.ubiobio.cl','G51taller','G51taller1058','G51taller_bd');
-    date_default_timezone_set('America/Santiago');
-    if(mysqli_connect_errno()){
-        echo 'Error de conexión:'.mysqli_connect_error();
-    }
+    // $conn=mysqli_connect('mysqltrans.face.ubiobio.cl','G51taller','G51taller1058','G51taller_bd');
+    // date_default_timezone_set('America/Santiago');
+    // if(mysqli_connect_errno()){
+    //    echo 'Error de conexión:'.mysqli_connect_error();
+    //}
 
     // Utilizar conexión local:
-    // $conn=mysqli_connect('db','root','test','unidep');
-    // if(mysqli_connect_errno()){
-    //     echo 'Error de conexión:'.mysqli_connect_error();
-    // }
+     $conn=mysqli_connect('db','root','test','unidep');
+     if(mysqli_connect_errno()){
+         echo 'Error de conexión:'.mysqli_connect_error();
+     }
 
     // Utilizar otra conexión:
     // $conn=mysqli_connect('host','username','password','dbname');
